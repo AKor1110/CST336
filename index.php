f888dfb101312a97e0c505d3a8c4bbfb9abd1929
Andy Kor <akor@csumb.edu>
2018-08-29 21:44:17 +0000

    updated index.php

--- index.php
+++ index.php
@@ -1,9 +1,10 @@
+<!DOCTYPE html>
+
 <html>
     
     <head>
         <meta charset = "utf-8">
         <title> Andy's CST336 Workspace </title>
-        <script src="https://code.jquery.com/jquery-1.12.0.min.js"></script>
     </head>
     
     <body>
