0e785a43a14eada594e9025ea69d876d26c5ff1d
Andy Kor <akor@csumb.edu>
2018-08-29 22:08:46 +0000

    Added new directories

---
+++ homework/homework_1/syntaxDirectedTranslation.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html>
+
+    <head>
+      <meta charset = "utf-8">
+      <title> Compiler Design: Syntax Analysis </title>
+      <link href = "css/styles.css" rel = "stylesheet" type = "text/css"/>
+      <script src="https://code.jquery.com/jquery-1.12.0.min.js"></script>
+      <script src="js/scripts.js"></script>
+
+    </head>
+    <body>
+        
+        <header> Syntax Directed Translation </header>
+        
+        <nav>
+            <a href = "index.php"> Introduction </a>
+            <a href = "lexicalAnalysis.php"> Lexical Analysis </a>
+            <a href = "syntaxAnalysis.php"> Syantax Analysis </a>
+            <a href = "syntaxDirectedTranslation.php"> Syntax Directed Translation </a>
+            <a href = "resources.php"> Resources </a>
+        </nav>
+        
+        
+
+        <footer>
+        
+            <hr>
+                CST 336 Internet Programming. 2018&copy; Kor <br />
+                <strong> Disclaimer: </strong>
+                The information in this webpage is fictitous. <br />
+                It is used for academic purposes only.
+    
+                <br />
+
+
+        </footer>
+
+    </body>
+
+</html>
