afbdabe574d028b2c662d11eb0d81bcbb5528e42
Andy Kor <akor@csumb.edu>
2018-08-29 21:54:14 +0000

    adding portfolio directory

--- portfolio/contact.php
+++ portfolio/contact.php
@@ -1,13 +1,20 @@
 <?php
 
 $name = $_POST["name"];
-$email = $_POST["email"];
+$email = strtolower($_POST["email"]);
 $address = $_POST["address"];
 $phone = $_POST["name"];
 $comments = $_POST["comments"];
 
 $data = array($name, $email, $address, $phone, $comments);
+$json = json_encode($data, JSON_NUMERIC_CHECK);
 
-echo json_encode($data, JSON_NUMERIC_CHECK);
+$file = fopen("messages/comments.txt", "a");
+fwrite($file, $json);
+fwrite($file, "\n");
+fclose($file);
+
+header("location: contact.html");
+
 
 ?>
