e504b277d0cd51580fc4551e701027bde6609e67
Andy Kor <akor@csumb.edu>
2018-09-12 21:13:27 +0000

    changed frame tag

--- homework/homework_1/syntaxAnalysis.php
+++ homework/homework_1/syntaxAnalysis.php
@@ -40,7 +40,7 @@
         
         <br />
         
-        <iframe width="560" height="315" src="https://www.youtube.com/embed/TkRy17gnq3E" allow="autoplay; encrypted-media" allowfullscreen></iframe>
+        <iframe width="560" height="315" src="https://www.youtube.com/embed/TkRy17gnq3E" frameborder = "0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
         
         <footer>
         
