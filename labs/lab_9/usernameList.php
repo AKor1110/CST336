48c73020a32ee57f6defe4ae8659f392af8d5e87
Andy Kor <akor@csumb.edu>
2018-11-26 23:01:45 +0000

    changed lab 9 api

--- labs/lab_9/usernameList.php
+++ labs/lab_9/usernameList.php
@@ -8,7 +8,7 @@ $namedParameters[":username"] = $_GET["username"];
 $sql = "SELECT * FROM om_admin WHERE 1 and username = :username";
 $stmt = $dbConn->prepare($sql);
 $stmt->execute($namedParameters);
-$records = $stmt->fetchAll(PDO::FETCH_ASSOC);
+$records = $stmt->fetch(PDO::FETCH_ASSOC);
 
 echo json_encode($records, JSON_NUMERIC_CHECK);
 ?>
